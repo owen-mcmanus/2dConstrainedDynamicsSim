4696c1b15816bad708077339cb4e1a3c97eaac3c
owen-mcmanus <owen.mcmanus727@gmail.com>
2023-07-15 20:46:09 -0700

    add animator

---
+++ animator.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import pandas as pd
+
+m = pd.read_csv('out/build/x64-Debug/data.csv')
+
+plt.style.use('seaborn')
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+t = m['time']
+x = m['mass1:PX']
+y = m['mass1:PY']
+
+
+def animation(i):
+    x = []
+    y = []
+    x = m[0:i]['mass1:PX']
+    y = m[0:i]['mass1:PY']
+
+    ax.clear()
+    ax.plot(x, y)
+
+
+ani = FuncAnimation(fig, animation, interval=3)
+plt.show()
